02d1597a369e3013cfbac1e70b0c2e4e7704e507
Andrew Dunn <andy.dunn@expensys.com>
2015-03-23 21:53:53 +0000

    Moved the running of the rules away from the plugin and added the first test

--- buildSrc/src/main/groovy/com/fizzpod/gradle/plugins/sweeney/SweeneyPlugin.groovy
+++ buildSrc/src/main/groovy/com/fizzpod/gradle/plugins/sweeney/SweeneyPlugin.groovy
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory
 
 import com.fizzpod.gradle.plugins.sweeney.rules.RuleDefinitionParserLoader
 import com.fizzpod.gradle.plugins.sweeney.rules.RuleLoader
+import com.fizzpod.gradle.plugins.sweeney.rules.RuleRunner
+import com.fizzpod.gradle.plugins.sweeney.rules.RunMode
 
 public class SweeneyPlugin implements Plugin<Project> {
 
@@ -17,67 +19,33 @@ public class SweeneyPlugin implements Plugin<Project> {
 	private RuleDefinitionParserLoader ruleDefinitionParserLoader = new RuleDefinitionParserLoader();
 	
 	void apply(Project project) {
+		
 		project.extensions.create("sweeney", SweeneyPluginExtension);
 		
 		project.afterEvaluate { proj -> 
 			LOGGER.info("Running rules after evaluation for project: {}", proj)
-			runEnforcementRules(proj);
-			runCautionRules(proj);
+			runEnforcementRules(proj, proj);
+			runCautionRules(proj, proj);
 		}
 		
 	}
 	
-	void runEnforcementRules(Project project) {
+	void runEnforcementRules(def project, def scope) {
 		SweeneyPluginExtension theSweeney = project.getExtensions().findByType(SweeneyPluginExtension)
-		LOGGER.info("Applying enforce rules: {}", theSweeney.enforceableRules);
-		applyRules(project, theSweeney.enforceableRules, true)
-	}
-	
-	void runCautionRules(Project project) {
-		SweeneyPluginExtension theSweeney = project.getExtensions().findByType(SweeneyPluginExtension)
-		LOGGER.info("Applying caution rules: {}", theSweeney.cautionaryRules);
-		applyRules(project, theSweeney.cautionaryRules, false)
-	}
-	
-	void applyRules(def scope, def ruleDefinitions, def enforce) {
 		
-		ruleDefinitions.each { ruleDefinition ->
-			LOGGER.debug("Applying rule: {}", ruleDefinition);
-			convertAndApplyRule(scope, ruleDefinition, enforce);
-		}
+		RuleRunner runner = new RuleRunner(RunMode.ENFORCE);
+		
+		runner.applyRules(scope, theSweeney.enforceableRules)
 	}
 	
-	void convertAndApplyRule(def scope, def ruleDefinition, def enforce) {
-		def convertedRuleDefinition = null;
-		ruleDefinitionParserLoader.all().each { it -> 
-			 convertedRuleDefinition = it.parse(ruleDefinition);
-		}
-		if(convertedRuleDefinition != null) {
-			applyRule(scope, convertedRuleDefinition, enforce);
-		} else {
-			LOGGER.error("Unable to parse rule: {}", ruleDefinition);
-			throw new IllegalArgumentException("Unable to parse rule: " + ruleDefinition);
-		}
+	void runCautionRules(def project, def scope) {
+		SweeneyPluginExtension theSweeney = project.getExtensions().findByType(SweeneyPluginExtension)
+		
+		RuleRunner runner = new RuleRunner(RunMode.CAUTION);
+		
+		runner.applyRules(scope, theSweeney.cautionaryRules)
+		
 	}
 	
-	void applyRule(def scope, def ruleDefinition, def enforce) {
-		ruleLoader.all().each { it ->
-			LOGGER.debug("Checking whether rule {} accepts definition {}", it.getType(), ruleDefinition);
-			if(it.accept(ruleDefinition)) {
-				LOGGER.debug("Testing definition {} with rule {}", ruleDefinition, it.getType())
-				try {
-					it.validate(scope, ruleDefinition);
-				} catch(AssertionError e) {
-					if(enforce) {
-						throw e;
-					} else {
-						LOGGER.warn("Warning: {}", e.getMessage());
-					}
-				}
-			} else {
-				LOGGER.debug("Rule {} rejected definition {}", it.getType(), ruleDefinition);
-			}
-		}
-	}
 	
 }
---
+++ buildSrc/src/test/groovy/com/fizzpod/gradle/plugins/sweeney/rules/RuleDefinitionParserLoaderSpec.groovy
@@ -0,0 +1,17 @@
+package com.fizzpod.gradle.plugins.sweeney.rules
+
+import nebula.test.ProjectSpec
+
+class RuleDefinitionParserLoaderSpec extends ProjectSpec {
+	
+	def 'rule definition parser loader should load parsers from service loader'() {
+		when:
+		def loader = new RuleDefinitionParserLoader();
+		
+		then:
+		def parsers = loader.all();
+		parsers != null
+		parsers.size() == 1
+	}
+	
+}
