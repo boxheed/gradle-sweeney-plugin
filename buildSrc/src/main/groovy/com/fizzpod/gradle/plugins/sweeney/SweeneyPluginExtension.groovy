795d4f2d634267b6330e200d98da6da3844c6ab7
boxheed <boxheed@users.noreply.github.com>
2015-03-23 09:59:57 +0000

    Added support for defining enforcement rules and cautionary rules.

--- buildSrc/src/main/groovy/com/fizzpod/gradle/plugins/sweeney/SweeneyPluginExtension.groovy
+++ buildSrc/src/main/groovy/com/fizzpod/gradle/plugins/sweeney/SweeneyPluginExtension.groovy
@@ -2,4 +2,12 @@ package com.fizzpod.gradle.plugins.sweeney;
 
 public class SweeneyPluginExtension {
 	
+	void enforce(def args) {
+		println(args)
+	}
+	
+	void caution(def args) {
+		println(args)
+	}
+	
 }
