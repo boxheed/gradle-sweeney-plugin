960f4feb7db04df356f0edb8bd69216b8da7bd93
boxheed <boxheed@users.noreply.github.com>
2015-03-23 23:05:16 +0000

    Added rule loader test

---
+++ buildSrc/src/test/groovy/com/fizzpod/gradle/plugins/sweeney/rules/RuleLoaderSpec.groovy
@@ -0,0 +1,17 @@
+package com.fizzpod.gradle.plugins.sweeney.rules
+
+import nebula.test.ProjectSpec
+
+class RuleLoaderSpec extends ProjectSpec {
+	
+	def 'rule loader should load rules from service loader'() {
+		when:
+		def loader = new RuleLoader();
+		
+		then:
+		def parsers = loader.all();
+		parsers != null
+		parsers.size() == 1
+	}
+	
+}
