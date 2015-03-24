6f864497996c3dcb504790f6c9b0231af6a4af85
Andrew Dunn <andy.dunn@expensys.com>
2015-03-24 22:21:46 +0000

    Added version range rule

--- buildSrc/src/main/groovy/com/fizzpod/gradle/plugins/sweeney/rules/Rule.groovy
+++ buildSrc/src/main/groovy/com/fizzpod/gradle/plugins/sweeney/rules/Rule.groovy
@@ -3,6 +3,8 @@ package com.fizzpod.gradle.plugins.sweeney.rules
 interface Rule {
 	
 	public static final String TYPE_ATTRIBUTE = "type"
+	public static final String EXPECT_ATTRIBUTE = "expect"
+	public static final String VALUE_ATTRIBUTE = "value"
 	
 	String getType();
 	
