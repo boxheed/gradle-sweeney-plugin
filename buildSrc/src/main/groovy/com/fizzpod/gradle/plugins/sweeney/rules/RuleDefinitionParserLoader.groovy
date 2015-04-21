4fff0cc62004b6773aa3e38be1e1772a76ec0ad1
boxheed <boxheed@users.noreply.github.com>
2015-04-21 15:36:33 +0100

    Rather than replying on the service loader am specifying defaults to be added on to end of list of parsers.

--- buildSrc/src/main/groovy/com/fizzpod/gradle/plugins/sweeney/rules/RuleDefinitionParserLoader.groovy
+++ buildSrc/src/main/groovy/com/fizzpod/gradle/plugins/sweeney/rules/RuleDefinitionParserLoader.groovy
@@ -2,11 +2,16 @@ package com.fizzpod.gradle.plugins.sweeney.rules;
 
 public class RuleDefinitionParserLoader {
 
+	private static final DEFAULT_PARSERS = [
+		new StringRuleDefinitionParser(),
+		new MapRuleDefinitionParser()
+	]
+
 	private static ServiceLoader<RuleDefinitionParser> ruleDefinitionParserServiceLoader = ServiceLoader
 			.load(RuleDefinitionParser.class);
 
 	def all () {
-		return ruleDefinitionParserServiceLoader.asList();
+		return ruleDefinitionParserServiceLoader.asList() + DEFAULT_PARSERS;
 	}
 	
 }
