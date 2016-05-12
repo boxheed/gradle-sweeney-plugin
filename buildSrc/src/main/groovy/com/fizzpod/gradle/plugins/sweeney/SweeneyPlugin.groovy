062a6bed95ab3ad55ef7d6b618e02447b00a688e
boxheed <boxheed@users.noreply.github.com>
2016-05-12 23:08:01 +0100

    #20 have added support for an explicit call to validate on the sweeney closure

--- buildSrc/src/main/groovy/com/fizzpod/gradle/plugins/sweeney/SweeneyPlugin.groovy
+++ buildSrc/src/main/groovy/com/fizzpod/gradle/plugins/sweeney/SweeneyPlugin.groovy
@@ -5,47 +5,23 @@ import org.gradle.api.Project
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-import com.fizzpod.gradle.plugins.sweeney.rules.RuleDefinitionParserLoader
-import com.fizzpod.gradle.plugins.sweeney.rules.RuleLoader
-import com.fizzpod.gradle.plugins.sweeney.rules.RuleRunner
-import com.fizzpod.gradle.plugins.sweeney.rules.RunMode
-
 public class SweeneyPlugin implements Plugin<Project> {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(SweeneyPlugin.class);
 
-	private RuleLoader ruleLoader = new RuleLoader();
-	
-	private RuleDefinitionParserLoader ruleDefinitionParserLoader = new RuleDefinitionParserLoader();
-	
 	void apply(Project project) {
 		
-		project.extensions.create("sweeney", SweeneyPluginExtension);
+		project.extensions.create("sweeney", SweeneyPluginExtension, project);
 		
 		project.afterEvaluate { proj -> 
 			LOGGER.info("Running rules after evaluation for project: {}", proj)
-			runEnforcementRules(proj, proj);
-			runCautionRules(proj, proj);
+			SweeneyPluginExtension theSweeney = project.getExtensions().findByType(SweeneyPluginExtension)
+			theSweeney.validate(proj);
 		}
 		
 	}
 	
-	void runEnforcementRules(def project, def scope) {
-		SweeneyPluginExtension theSweeney = project.getExtensions().findByType(SweeneyPluginExtension)
-		
-		RuleRunner runner = new RuleRunner(RunMode.ENFORCE);
-		
-		runner.applyRules(theSweeney.enforceableRules, scope)
-	}
 	
-	void runCautionRules(def project, def scope) {
-		SweeneyPluginExtension theSweeney = project.getExtensions().findByType(SweeneyPluginExtension)
-		
-		RuleRunner runner = new RuleRunner(RunMode.CAUTION);
-		
-		runner.applyRules(theSweeney.cautionaryRules, scope)
-		
-	}
 	
 	
 }
